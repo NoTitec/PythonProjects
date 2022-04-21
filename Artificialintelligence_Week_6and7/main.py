8077c7f65d6cc7f5b9d722ce9d2937c738b174f5
NoTitec <zkxkr0@gmail.com>
2022-04-21 16:43:24 +0900

    svm

--- Artificialintelligence_Week_6and7/main.py
+++ Artificialintelligence_Week_6and7/main.py
@@ -1,14 +1,22 @@
 import numpy as np
 from sklearn import datasets
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split, cross_val_predict
 from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import PolynomialFeatures
 
+import datetime
+now=datetime.datetime.now()
+print('20191320 권순혁 현재시간:{}'.format(now))
+
 #svm분류기
+from sklearn.tree import DecisionTreeClassifier
+
 iris = datasets.load_iris()
 X = iris["data"][:, (2, 3)] # petal length, petal width
 y = (iris["target"] == 2).astype(np.float64) # Iris-Virginica
@@ -48,9 +56,22 @@ rbf_kernel_svm_clf.fit(X_train, y_train)
 y_pred4=svm_clf.predict(X)#샘플데이터 예측
 print(accuracy_score(y,y_pred4))#예측 점수 출력
 
-clfone = OneVsOneClassifier(LinearSVC(random_state=0)).fit(X_train, y_train)
-clfrest = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X_train, y_train)
-predictedone = clfone.predict(X_test[:10])
-print(predictedone)
-predictedrest = clfrest.predict(X_test[:10])
-print(predictedrest)
+clfone = OneVsOneClassifier(LinearSVC(random_state=0,max_iter=1000000)).fit(X_train, y_train)
+clfrest = OneVsRestClassifier(LinearSVC(random_state=0,max_iter=1000000)).fit(X_train, y_train)
+predictedone = clfone.predict(X_test)
+print(accuracy_score(y_test,predictedone))
+predictedrest = clfrest.predict(X_test)
+print(accuracy_score(y_test,predictedrest))
+
+#Decision Tree
+
+iris = load_iris()
+print(iris.feature_names)
+X = iris.data[:, 2:] # petal length and width
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+tree_clf = DecisionTreeClassifier(max_depth=3)
+tree_clf.fit(X_train, y_train)
+y_pred=tree_clf.predict(X_test)#샘플데이터 예측
+print(accuracy_score(y_test,y_pred))#예측 점수 출력
